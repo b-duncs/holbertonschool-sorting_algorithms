3adaf553c4c3ab406215fc8d6102405caa351b00
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2023-01-11 19:28:29 -0600

    added files

--- sort.h
+++ sort.h
@@ -1,3 +1,9 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -11,3 +17,14 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void merge(int *array, int l, int m, int r, int *final_array)
+void call_to_sort(int *array, int l, int r, int *final_array)
+void merge_sort(int *array, size_t size)
+int max(int *a, int n, int i, int j, int k)
+void sift(int *a, int n, int i)
+void heap_sort(int *array, size_t size)
+
+#endif
